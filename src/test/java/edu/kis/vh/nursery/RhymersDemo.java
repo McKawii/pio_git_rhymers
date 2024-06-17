5285bc707371a955a1e9ecce197f97f77cea0e85
McKawii <kaweckimarcin999@gmail.com>
2024-06-18 00:03:29 +0200

    6.1 Dodanie stałych do testów jednostkowych.

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -6,20 +6,23 @@ import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.RhymersFactory;
 
 class RhymersDemo {
-
+    public static final int BOUND = 20;
+    public static final int RHYMES_NUMBER = 3;
+    public static final int ITERATION_NUMBER = 15;
     public static void main(String[] args) {
+
         RhymersFactory factory = new DefaultRhymersFactory();
         
         DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
         
-        for (int i = 1; i < 15; i++)
-            for (int j = 0; j < 3; j++)
+        for (int i = 1; i < ITERATION_NUMBER; i++)
+            for (int j = 0; j < RHYMES_NUMBER; j++)
                 rhymers[j].countIn(i);
         
         java.util.Random rn = new java.util.Random();
-        for (int i = 1; i < 15; i++)
-            rhymers[3].countIn(rn.nextInt(20));
+        for (int i = 1; i < ITERATION_NUMBER; i++)
+            rhymers[RHYMES_NUMBER].countIn(rn.nextInt(BOUND));
         
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
@@ -28,7 +31,7 @@ class RhymersDemo {
         }
         
         System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[3]).reportRejected());
+                + ((HanoiRhymer) rhymers[RHYMES_NUMBER]).reportRejected());
         
     }
     
